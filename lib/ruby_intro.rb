be5985aeef84ec0f80c5cbef5648534b15d38994
Gavin Goerke <40189873+gjgoerke@users.noreply.github.com>
2020-02-20 15:17:14 +0100

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,68 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    _total = 0
+    arr.each { |x| _total += x }
+    return _total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    return sum arr.sort!.reverse![0,2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    for i in arr
+        for j in arr
+            if i + j == n and i != j
+                return true
+            end
+        end
+    end
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return 'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    return !s.empty? && !!(s[0].match(/[bcdfghijklmnpqrstvqxyzBCDFGHIJKLMNPQRSTVWXYZ]/))
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s.match(/[^01]/) || s.empty?
+        return false 
+    end
+    s.reverse!
+    num = 0
+    for i in 0..(s.length) do
+        if(s[i] == "1") 
+            num += 2**i
+        end
+    end
+    return num.modulo(4).zero?
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    def initialize(isbn, price) 
+        raise ArgumentError unless isbn.is_a? String
+        raise ArgumentError if isbn.empty?
+        raise ArgumentError unless price > 0
+        @isbn = isbn
+        @price = price
+    end
+
+    attr_reader :isbn
+    attr_reader :price
+    attr_writer :isbn
+    attr_writer :price
+
+    def price_as_string 
+        return sprintf("$%2.2f", @price)
+    end
 end
+
